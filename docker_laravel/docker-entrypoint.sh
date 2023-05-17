b93b43972e27a915754acaee7f2c9c14d15398fb
Tri Do <tri.do@elinext.com>
2023-05-17 12:47:03 +0200

    update

--- docker_laravel/docker-entrypoint.sh
+++ docker_laravel/docker-entrypoint.sh
@@ -4,5 +4,8 @@ cd /var/www/html/
 git clone -b main https://cicd:$GITLAB_TOKEN@gitlab.com/evobio/evobio.link.git
 cd /var/www/html/evobio.link/ && composer install
 chown -R www-data:www-data /var/www/html/evobio.link/storage
+chown -R www-data:www-data /var/www/html/evobio.link/bootstrap
+ln -s /app/.env /var/www/html/evobio.link/
+echo 'Contents' > /var/www/html/evobio.link/storage/app/public/installed;
 
 exec "$@"
